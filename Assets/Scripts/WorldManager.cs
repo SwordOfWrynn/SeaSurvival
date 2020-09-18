22c7b848838635eaf203967e40f982eaec6cd701
SwordOfWrynn <bubba.wiens@gmail.com>
2020-09-17 17:23:25 -0700

    Added render pipeline asset and post processing asset

---
+++ Assets/Scripts/WorldManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class WorldManager : MonoBehaviour
+{
+
+    public static WorldManager Instance { get; protected set; }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(Instance != null)
+        {
+            UnityEngine.Debug.Log("There is more than 1 WorldManager in the Scene!");
+            return;
+        }
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
